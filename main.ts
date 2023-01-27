8a2f0ba0a9d39825ba336031f6ad8d5563574d6a
Antoniopro-hue <63246009+Antoniopro-hue@users.noreply.github.com>
2023-01-27 12:34:32 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,12 @@ let control2 = 0
 basic.forever(function () {
     while (control2 < 1024) {
         control2 += 1
-        pins.digitalWritePin(DigitalPin.P0, control2)
+        pins.analogWritePin(AnalogPin.P0, control2)
         basic.pause(5000)
     }
     while (control2 > 0) {
         control2 += -1
-        pins.digitalWritePin(DigitalPin.P0, control2)
+        pins.analogWritePin(AnalogPin.P0, control2)
         basic.pause(5000)
     }
 })
